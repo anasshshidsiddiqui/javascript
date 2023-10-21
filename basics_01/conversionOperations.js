0f0204370e9dae64fbfbea182fd1df9559bc96c1
anasshshidsiddiqui <44909627+anasshshidsiddiqui@users.noreply.github.com>
2023-10-21 11:18:46 +0000

    conversion data type

---
+++ basics_01/conversionOperations.js
@@ -0,0 +1,14 @@
+let score = "33abc";
+console.log(typeof(score));
+let numberInValue = Number(score);
+console.log(typeof(numberInValue));
+console.log(numberInValue);
+
+let numberVariable = 33;
+numberVariable = String(numberVariable);
+console.log(numberVariable);
+console.log(typeof(numberVariable));
+
+console.log(typeof(1 + "2"));
+console.log(null > 0);
+console.log(typeof(null));

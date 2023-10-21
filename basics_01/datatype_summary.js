c69cbb3258cbfe8298ce08b68b3ec44f0479dba7
anasshshidsiddiqui <44909627+anasshshidsiddiqui@users.noreply.github.com>
2023-10-21 18:13:55 +0000

    Summary and maths numbers

---
+++ basics_01/datatype_summary.js
@@ -0,0 +1,11 @@
+console.log("Anas");
+
+let obj1 = {
+    email: "anasshahid@gmail.com"
+};
+
+let user = obj1;
+user.email = "anas@gmail.com";
+
+console.log(obj1);
+console.log(user);
---
+++ basics_01/mathsAndNumbers.js
@@ -0,0 +1,16 @@
+let score = 400;
+//console.log(score);
+
+let balance = new Number(100);
+//console.log(balance.toFixed(2));
+
+let real = 1123.8956;
+//console.log(real.toPrecision(3).length);
+
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-PK'));
+
+let date = new Date();
+//console.log(date.toLocaleString());
+
+////////////////////// Maths ///////////////////////////////
